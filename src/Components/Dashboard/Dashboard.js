a38f33a9d3349aff0c2ffe2a5dd0f5bf0939bd9d
Lili <ravalidesign@gmail.com>
2022-09-23 16:53:16 -0500

    (feat)added filtering functionality

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,27 +1,48 @@
+import { useState } from "react";
 import "./Dashboard.css";
 import { Card } from "../Card/Card";
 import Students from "../../JSON/hp.students.json";
+import Staff from "../../JSON/hp.staff.json";
 
-console.log(Students.map((item) => item));
 export const Dashboard = () => {
+  const [isStudent, setIsstudent] = useState(true);
+
+  const handleFilter = (status) => {
+    setIsstudent(status);
+  };
+
   return (
     <>
-      <button>Estudiantes</button>
-      <button>Staff</button>
+      <button onClick={() => handleFilter(true)}>Estudiantes</button>
+      <button onClick={() => handleFilter(false)}>Staff</button>
       <div className="dashboard-content">
-        {Students.map((item) => (
-          <Card
-            key={item.name}
-            status={item.alive ? "vivo" : "finado"}
-            degree={item.hogwartsStudent ? "Estudiante" : "Staff"}
-            image={item.image}
-            name={item.name}
-            dateOfBirth={item.dateOfBirth}
-            gender={item.gender}
-            eyeColour={item.eyeColour}
-            hairColour={item.hairColour}
-          />
-        ))}
+        {isStudent
+          ? Students.map((item) => (
+              <Card
+                key={item.name}
+                status={item.alive ? "vivo" : "finado"}
+                degree={item.hogwartsStudent ? "Estudiante" : "Staff"}
+                image={item.image}
+                name={item.name}
+                dateOfBirth={item.dateOfBirth}
+                gender={item.gender}
+                eyeColour={item.eyeColour}
+                hairColour={item.hairColour}
+              />
+            ))
+          : Staff.map((item) => (
+              <Card
+                key={item.name}
+                status={item.alive ? "vivo" : "finado"}
+                degree={item.hogwartsStudent ? "Estudiante" : "Staff"}
+                image={item.image}
+                name={item.name}
+                dateOfBirth={item.dateOfBirth}
+                gender={item.gender}
+                eyeColour={item.eyeColour}
+                hairColour={item.hairColour}
+              />
+            ))}
       </div>
     </>
   );
